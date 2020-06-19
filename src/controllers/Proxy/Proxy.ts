d92f00cd016c088fed40b4a64240646f4d9f2f03
Ali Turab Abbasi <Ali-Turab.Abbasi@t-systems.com>
2020-06-19 18:56:48 +0200

    impolement update endpoint

--- src/controllers/Proxy/Proxy.ts
+++ src/controllers/Proxy/Proxy.ts
@@ -6,10 +6,9 @@ export class ProxyController extends CrudController {
 
     public create(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
         const params: ProxyInterface = req.body;
-
         Proxy.create<Proxy>(params)
-          .then((proxy: Proxy) => res.status(201).json(proxy))
-          .catch((err: Error) => res.status(500).json(err));
+            .then((proxy: Proxy) => res.status(201).json(proxy))
+            .catch((err: Error) => res.status(500).json(err));
     }
 
     public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
@@ -19,17 +18,27 @@ export class ProxyController extends CrudController {
     }
 
     public update(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-        throw new Error("Method not implemented.");
+        const params: ProxyInterface = req.body.obj;
+        const id: string = req.body.id;
+        Proxy.update<Proxy>(params, {
+            where: {
+                id: id
+            }
+        })
+            .then((proxy) => res.sendStatus(200))
+            .catch((err: Error) => res.status(500).json(err));
     }
 
     public delete(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-        console.log('I am trying')
         Proxy.destroy({
             where: {
                 id: req.body.id
-            } 
-        }).then((resp)=>{
-            res.status(200).json(resp);
+            }
+        }).then((resp) => {
+            if (resp)
+                res.status(200).json(resp);
+            else
+                res.sendStatus(404);
         })
     }
 }
