79576ac71031cece0ac4885ada981126cdce79f7
Ali Turab Abbasi <Ali-Turab.Abbasi@t-systems.com>
2020-06-20 18:46:40 +0200

    configured 2 proxies

--- src/controllers/Proxy/ProxyController.ts
+++ src/controllers/Proxy/ProxyController.ts
@@ -23,44 +23,74 @@ export class ProxyController extends CrudController {
     }
 
     public async refresh(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response) {
-        res.json(await this.getData())
-//   return await this.getData();
+        let url1 = 'https://free-proxy-list.net/';
+        let url2 = 'https://www.netzwelt.de/proxy/index.html';
+        let p1 = await this.getData(url1);
+        let $ = cheerio.load(p1)
+
+        let result = $(".table-responsive > table.table-striped > tbody > tr").map((i, element) => ({
+            ip: $(element).find('td:nth-of-type(1)').text().trim()
+            , port: $(element).find('td:nth-of-type(2)').text().trim()
+            , code: $(element).find('td:nth-of-type(3)').text().trim()
+            , country: $(element).find('td:nth-of-type(4)').text().trim()
+            , https: $(element).find('td:nth-of-type(7)').text().trim()
+            , provider: url1
+        })).get()
+        let params: ProxyInterface[] = result;
+        result.forEach(async element => {
+            let a = await Proxy.findByPk(element.ip)
+            if (!a) {
+                let resp = await Proxy.create(element)
+            }
+            else {
+                await a.changed('updatedAt', true)
+
+            }
+        })
+
+        let p2 = await this.getData(url2);
+         $ = cheerio.load(p2)
+
+         result = $("div.tblc > table > tbody > tr").map((i, element) => ({
+             
+            ip: $(element).find('td:nth-of-type(1)').text().trim()
+            , port: $(element).find('td:nth-of-type(2)').text().trim()
+            , code: $(element).find('td:nth-of-type(3)').text().trim()
+            , country: $(element).find('td:nth-of-type(3)').text().trim()
+            , https: $(element).find('td:nth-of-type(5)').text().trim()
+            , provider: url2
+        })).get()
+          params = result;
+          console.log(result)
+        result.forEach(async element => {
+            let a = await Proxy.findByPk(element.ip)
+            if (!a) {
+                let resp = await Proxy.create(element)
+                // console.log('this is resp ', resp);
+            }
+            else {
+                await a.changed('updatedAt', true)
+
+            }
+        })
+        console.log('size is ', params.length);
+        res.json(result);
+
+        //   return await this.getData();
 
     }
 
-    public getData() {
-        console.log('inside get Data')
+    public async getData(proxy: string) {
         const options = {
-            url: 'https://free-proxy-list.net/',
+            url: proxy,
             method: 'GET',
         }
 
+        return await request.get(options);
+
         request.get(options).then((html) => {
 
-            const $ = cheerio.load(html)
 
-            const result = $(".table-responsive > table.table-striped > tbody > tr").map((i, element) => ({
-                ip: $(element).find('td:nth-of-type(1)').text().trim()
-                , port: $(element).find('td:nth-of-type(2)').text().trim()
-                , code: $(element).find('td:nth-of-type(3)').text().trim()
-                , country: $(element).find('td:nth-of-type(4)').text().trim()
-                , https: $(element).find('td:nth-of-type(7)').text().trim()
-                , provider: options.url
-            })).get()
-            const params: ProxyInterface[] = result;
-            result.forEach(async element => {
-                let a = await Proxy.findByPk(element.ip)
-                if (!a) {
-                    let resp = await Proxy.create(element)
-                    // console.log('this is resp ', resp);
-                }
-                else {
-                     await a.changed('updatedAt', true)
-
-                }
-            })
-            console.log('size is ', params.length)
-            return params;
         });
 
     }
